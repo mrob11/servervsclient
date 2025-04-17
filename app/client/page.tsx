771b255cb733e989f151b73c570a3faaf766741e
Mike Robinson <mike.robinson@globacore.com>
2025-04-17 11:38:22 -0400

    Enhance ClientPage with additional explanation and update ESLint config for better rule handling

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -2,10 +2,16 @@ import { Characters } from "./characters"
 
 export default function ClientPage() {
   return (
-    <div className="border-2 border-emerald-500 p-8">
+    <div className="border-2 border-emerald-500 p-8 space-y-4">
       <p>
         On this page we're going to fetch some data client-side. You can verify
         this by looking at the network tab in your dev inspector.
+      </p>
+      <p>
+        If the data fetching code inside the <code>&lt;Characters /&gt;</code>{" "}
+        component tried to import from the <code>@lib/ram-serveronly</code>{" "}
+        module, there would've been an error, because it's marked with{" "}
+        <code>import "server-only"</code>.
       </p>
       <Characters />
     </div>
